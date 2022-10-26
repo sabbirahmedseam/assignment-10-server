5763e3415d0e2ce7099a5c5cd335578c26d42044
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-10-26 16:35:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/", (req, res) => {
+  res.send("Everything is prepared");
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectCourse = courses.find((course) => course.id === id);
+  res.send(selectCourse);
+});
+
+app.listen(port, () => {
+  console.log("server side running", port);
+});
